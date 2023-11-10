25fa32215ac42909e056fea6f469265494b0ae08
abhishek2young <135578020+abhishek2young@users.noreply.github.com>
2023-11-10 20:33:12 +0530

    temaplates added

--- app.py
+++ app.py
@@ -1,36 +1,45 @@
-from flask import Flask,jsonify,request,render_template,url_for
+from flask import Flask, jsonify, request, render_template, url_for
 import pickle
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-    return "default API" 
+    return render_template("index.html")
+
 
 @app.route("/welcome")
 def index1():
     return "Welcome to Flask"
 
-@app.route("/predict")
+
+@app.route("/predict", methods=["POST"])
 def predict_flower():
+    
 
-    with open("model.pkl","rb") as model:
-        ml_model=pickle.load(model)
+    with open("model.pkl", "rb") as model:
+        ml_model = pickle.load(model)
+    
+    data=request.form
+    print("Data is :",data)
 
-    SepalLengthCm = 6
-    SepalWidthCm = 4.8
-    PetalLengthCm = 5.2
-    PetalWidthCm = 3.75
+    SepalLengthCm = eval(data["SepalLengthCm"])
+    SepalWidthCm = eval(data["SepalWidthCm"])
+    PetalLengthCm = eval(data["PetalLengthCm"])
+    PetalWidthCm = eval(data["PetalWidthCm"])
 
-    result = ml_model.predict([[SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm]])
-    if result[0]==2:
-        iris_flower="iris_virginica"
-    if result[0]==0:
-        iris_flower="iris_setosa"
-    if result[0]==1:
-        iris_flower="iris_versicolor"
+    result = ml_model.predict(
+        [[SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm]]
+    )
+    if result[0] == 2:
+        iris_flower = "iris_virginica"
+    if result[0] == 0:
+        iris_flower = "iris_setosa"
+    if result[0] == 1:
+        iris_flower = "iris_versicolor"
 
-    return jsonify({"result":f"Flower is {iris_flower}"})
+    return jsonify({"result": f"Flower is {iris_flower}"})
 
 
 if __name__ == "__main__":
